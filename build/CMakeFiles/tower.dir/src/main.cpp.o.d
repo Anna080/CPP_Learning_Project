087958690c5a99bf6d79a5c74ef2d5ed7a1e8908
Anna080 <annadiaw2000@gmail.com>
2022-04-17 00:45:00 +0200

    first commit

---
+++ build/CMakeFiles/tower.dir/src/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/tower.dir/src/main.cpp.o: \
+  /Users/annadiaw/Documents/Etudes/M1/C++/CPP_Learning_Project/src/main.cpp \
+  /Users/annadiaw/Documents/Etudes/M1/C++/CPP_Learning_Project/src/tower_sim.hpp
